69fb943c733d42c7a76fa309c5119c7a0fd7f982
SQUARE-WAVES <michael.behrman@gmail.com>
2017-08-08 01:29:23 -0500

    added promise interface for promises

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-var afsm = require("async-fsm");
-var parser_tasks = require("./lib/parser_tasks.js");
+const afsm = require("async-fsm");
+const parser_tasks = require("./lib/parser_tasks.js");
+const promise_tasks = require("./lib/parser_tasks.js").promise_tasks;
 
-var parser_fsm = afsm(parser_tasks);
+const parser_fsm = afsm(parser_tasks);
+const promise_fsm = afsm(promise_tasks);
 
 module.exports = function (input, tag_function, cb) {
   parser_fsm("input", input, tag_function, cb);
 };
+
+module.exports.promise = function(input,tag_function){
+	return new Promise((resolve,reject) => {
+		promise_fsm("input",input,tag_function, (err,result) => {
+			if(err){
+				reject(err)
+			}
+			else{
+				resolve(result)
+			}
+		})
+	})
+}
